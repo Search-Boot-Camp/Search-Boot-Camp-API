18e92fa30f48b74938596a1128ad67464ddd28de
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-02-12 14:37:50 +0900

    update db

--- sbc_api/config/settings.py
+++ sbc_api/config/settings.py
@@ -106,7 +106,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sbc',
         'USER': 'root',
-        'PASSWORD': DB_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
