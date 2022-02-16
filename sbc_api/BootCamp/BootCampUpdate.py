9a47c6075be353b8a2a8ed4d5e8c23c11afac1c6
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-02-16 13:24:29 +0900

    modify model

--- sbc_api/BootCamp/BootCampUpdate.py
+++ sbc_api/BootCamp/BootCampUpdate.py
@@ -69,25 +69,114 @@ for idx in range(1, bootcamp_sheet.nrows):
     bootcamp_name = bootcamp_sheet.cell(idx, 3).value
     tech_stack = bootcamp_sheet.cell(idx, 4).value if bootcamp_sheet.cell(idx, 4).value else None
     price = bootcamp_sheet.cell(idx, 5).value if bootcamp_sheet.cell(idx, 5).value else None
-    training_period = bootcamp_sheet.cell(idx, 6).value
-    accept = bootcamp_sheet.cell(idx, 7).value
+    temp = bootcamp_sheet.cell(idx, 6).value
+    if temp == 1:
+        training_period = '1개월 미만'
+    elif temp == 2:
+        training_period = '1개월~3개월 미만'
+    elif temp == 3:
+        training_period = '3개월~6개월 미만'
+    elif temp == 4:
+        training_period = '6개월 이상'
+    else:
+        training_period = None
+
+    temp = bootcamp_sheet.cell(idx, 7).value
+    if temp == 1:
+        accept = '모집중'
+    elif temp == 2:
+        accept = '모집예정'
+    elif temp == 3:
+        accept = '모집완료'
+    else:
+        accept = None
+
     apply_start = datetime.datetime(
         *xlrd.xldate_as_tuple(bootcamp_sheet.cell(idx, 9).value, bootcamp_book.datemode)).strftime(
         "%x") if bootcamp_sheet.cell(idx, 8).value else None
     apply_end = datetime.datetime(
         *xlrd.xldate_as_tuple(bootcamp_sheet.cell(idx, 9).value, bootcamp_book.datemode)).strftime(
         "%x") if bootcamp_sheet.cell(idx, 9).value else None
-    on_offline = bootcamp_sheet.cell(idx, 10).value
-    place = bootcamp_sheet.cell(idx, 11).value
+    temp = bootcamp_sheet.cell(idx, 10).value
+    if temp == 1:
+        on_offline = 'Online'
+    elif temp == 2:
+        on_offline = 'Offline'
+    elif temp == 3:
+        on_offline = 'Both'
+    else:
+        on_offline = None
+
+    temp = bootcamp_sheet.cell(idx, 11).value
+    if temp == 1:
+        place = '없음'
+    elif temp == 2:
+        place = '서울'
+    elif temp == 3:
+        place = '경기'
+    elif temp == 4:
+        place = '대구'
+    elif temp == 5:
+        place = '부산'
+    elif temp == 6:
+        place = '울산'
+    elif temp == 7:
+        place = '광주'
+    elif temp == 8:
+        place = '대전'
+    elif temp == 9:
+        place = '경북'
+    elif temp == 10:
+        place = '경남'
+    elif temp == 11:
+        place = '전북'
+    elif temp == 12:
+        place = '전남'
+    elif temp == 13:
+        place = '강원'
+    elif temp == 14:
+        place = '충남'
+    else:
+        place = None
 
     apply_condition = list(map(int, map(float, str(bootcamp_sheet.cell(idx, 12).value).split(","))))
     apply_conditions = []
     for x in apply_condition:
-        apply_conditions += APPLYCONDITION[x-1][1] + ","
+        apply_conditions += APPLYCONDITION[x - 1][1] + ","
     apply_condition = "".join(apply_conditions)[:-1]
 
-    apply_course = bootcamp_sheet.cell(idx, 13).value
-    k_digital = bootcamp_sheet.cell(idx, 14).value
+    temp = bootcamp_sheet.cell(idx, 13).value
+    if temp == 1:
+        apply_course = '없음'
+    elif temp == 2:
+        apply_course = '코테'
+    elif temp == 3:
+        apply_course = '지원서'
+    elif temp == 4:
+        apply_course = '인터뷰'
+    elif temp == 5:
+        apply_course = '코테 + 지원서'
+    elif temp == 6:
+        apply_course = '코테 + 인터뷰'
+    elif temp == 7:
+        apply_course = '지원서 + 인터뷰'
+    elif temp == 8:
+        apply_course = '코테 + 지원서 + 인터뷰'
+    elif temp == 9:
+        apply_course = '적성검사'
+    else:
+        apply_course = None
+
+    temp = bootcamp_sheet.cell(idx, 14).value
+    if temp == 1:
+        k_digital = '국민내일배움카드 X'
+    elif temp == 2:
+        k_digital = '국민내일배움카드 필수'
+    elif temp == 3:
+        k_digital = '국민내일배움카드 선택사항'
+    else:
+        apply_course = None
+
     link = bootcamp_sheet.cell(idx, 15).value if bootcamp_sheet.cell(idx, 15).value else None
     note = bootcamp_sheet.cell(idx, 16).value if bootcamp_sheet.cell(idx, 16).value else None
     image_id = bootcamp_sheet.cell(idx, 17).value if bootcamp_sheet.cell(idx, 17).value else None
--- sbc_api/BootCamp/models.py
+++ sbc_api/BootCamp/models.py
@@ -1,69 +1,4 @@
-from re import search
 from django.db import models
-
-TRAININGPERIOD = (
-    (1, '1개월 미만'),
-    (2, '1개월~3개월 미만'),
-    (3, '3개월~6개월 미만'),
-    (4, '6개월 이상'),
-)
-
-
-ACCEPT = (
-    (1, '모집중'),
-    (2, '모집예정'),
-    (3, '모집완료'),
-)
-
-ONOFFLINE = (
-    (1, 'Online'),
-    (2, 'Offline'),
-    (3, 'Both'),
-)
-
-PLACE = (
-    (1, '없음'),
-    (2, '서울'),
-    (3, '경기'),
-    (4, '대구'),
-    (5, '부산'),
-    (6, '울산'),
-    (7, '광주'),
-    (8, '대전'),
-    (9, '경북'),
-    (10, '경남'),
-    (11, '전북'),
-    (12, '전남'),
-    (13, '강원'),
-    (14, '충남'),
-)
-
-APPLYCONDITION = (
-    ('1', '졸업예정자'),
-    ('2', '전공자'),
-    ('3', '누구나'),
-    ('4', '직장인'),
-    ('5', '예비창업자'),
-    ('6', '졸업자'),
-)
-
-APPLYCOURSE = (
-    (1, '없음'),
-    (2, '코테'),
-    (3, '지원서'),
-    (4, '인터뷰'),
-    (5, '코테 + 지원서'),
-    (6, '코테 + 인터뷰'),
-    (7, '지원서 + 인터뷰'),
-    (8, '코테 + 지원서 + 인터뷰'),
-    (9, '적성검사'),
-)
-
-KDIGITAL = (
-    (1, '국민내일배움카드 X'),
-    (2, '국민내일배움카드 필수'),
-    (3, '국민내일배움카드 선택사항'),
-)
 
 class BootCamp(models.Model):
     company_id = models.CharField(max_length=15, null=True, blank=True) # 사업자등록번호
@@ -72,15 +7,15 @@ class BootCamp(models.Model):
     bootcamp_name = models.CharField(max_length=100, null=True, blank=True) # 부트캠프 이름
     tech_stack = models.CharField(max_length=100, null=True, blank=True) # 기술 스택
     price = models.CharField(max_length=100, null=True, blank=True) # 수강료
-    training_period = models.IntegerField(choices=TRAININGPERIOD, null=True, blank=True) # 교육 기간
-    accept = models.IntegerField(choices=ACCEPT, null=True, blank=True) # 모집 현황
+    training_period = models.CharField(max_length=100, null=True, blank=True) # 교육 기간
+    accept = models.CharField(max_length=100, null=True, blank=True) # 모집 현황
     apply_start = models.CharField(max_length=100, null=True, blank=True) # 모집 시작일
     apply_end = models.CharField(max_length=100, null=True, blank=True) # 모집 마감일
-    on_offline = models.IntegerField(choices=ONOFFLINE, null=True, blank=True) # 온/오프라인
-    place = models.IntegerField(choices=PLACE, null=True, blank=True) # 장소
+    on_offline = models.CharField(max_length=100, null=True, blank=True) # 온/오프라인
+    place = models.CharField(max_length=100, null=True, blank=True) # 장소
     apply_condition = models.CharField(max_length=100, null=True, blank=True) # 지원자격
-    apply_course = models.IntegerField(choices=APPLYCOURSE, null=True, blank=True) # 지원 과정
-    k_digital = models.IntegerField(choices=KDIGITAL, null=True, blank=True) # 국민내일배움카드 여부
+    apply_course = models.CharField(max_length=100, null=True, blank=True) # 지원 과정
+    k_digital = models.CharField(max_length=100, null=True, blank=True) # 국민내일배움카드 여부
     link = models.CharField(max_length=1000, null=True, blank=True) # 링크
     note = models.CharField(max_length=1000, null=True, blank=True) # 비고
     image_id = models.CharField(max_length=15, null=True, blank=True) # 이미지 번호
